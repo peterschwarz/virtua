f304e0fc4e0d559e5587771cfbff7ac558eca081
Peter Schwarz <peter.schwarz@bicycle.io>
2019-10-13 18:02:51 -0500

    Replace custom test runner with figwheel autotest
    Remove the custom test runner and update the test utils to behave
    correctly during the use of the figwheel auto testing facility.
    
    Signed-off-by: Peter Schwarz <peter.schwarz@bicycle.io>

--- test/virtua/test_utils.cljs
+++ test/virtua/test_utils.cljs
@@ -2,8 +2,9 @@
   (:require [goog.dom :as gdom]))
 
 (defn new-container! [id]
-  (let [component (gdom/createDom "div" #js {:id id})]
-    (gdom/appendChild (gdom/getElement "tests") component)
+  (let [component (gdom/createDom "div" #js {:id id})
+        body (-> (gdom/getElementsByTagName "body") (aget 0))]
+    (gdom/appendChild body component)
     component))
 
 (defn remove-container! [el]
