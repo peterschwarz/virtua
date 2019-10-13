74736de045a91e110ac90f4c445173949d3c08aa
Peter Schwarz <peter.schwarz@bicycle.io>
2019-10-13 18:09:22 -0500

    Remove bootstrap
    Additionally, restructure the demo component to use HTML5 header and
    section elements.
    
    Signed-off-by: Peter Schwarz <peter.schwarz@bicycle.io>

--- dev/virtua/main.cljs
+++ dev/virtua/main.cljs
@@ -6,27 +6,44 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:content "Hello world!"}))
+(defonce app-state (atom {:content "Hello world!" :checked? true}))
 
 (defn a-component [state]
-  [:div.row
+  [:section#sub-component
    [:p "I'm a sub component"]
    [:p "My state of interest: " (:content state)]])
 
 (v/attach!
   (fn [state]
-    [:div.container
-     [:h1 "Hello, Virtua"]
-     [:p "Hello there, we're using Virtua to render this page."]
-     a-component
+    [:div
+     [:header [:h1 "Hello, Virtua"]]
+
+     [:section#demo-description
+      [:article "Hello there, we're using Virtua to render this page."]]
+
+      a-component
+
+    [:section#item-list
      [:ul
-      (map (fn [i] [:li i]) (:list state))]
-     [:div.row
-      [:div.col-xs-1 (get state :count 0)]
-      [:div.col-xs-1
-       [:button.btn.btn-default
-        {:on-click #(swap! app-state update-in [:count] inc)}
-        "Count!" ]]]])
+      (map (fn [i] [:li i]) (:list state))]]
+
+     [:section#counter-demo
+      (let [checked? (get state :enabled? true)]
+        [:div#counter
+         [:div#count (get state :count 0)]
+         [:div#count-button
+          [:button
+           {:on-click #(swap! app-state update-in [:count] inc)
+            :disabled (not checked?)}
+           "Count!" ]]
+         [:div#count-enabled
+          [:label
+           [:input {:type "checkbox"
+                    :checked checked?
+                    :on-change #(swap! app-state assoc :enabled? (not checked?))}]
+           "Enable Counting"]]])]]
+      )
+
   app-state
   (. js/document (getElementById "app")))
 
