e0b8907c85cd1d00ec645edc6e6fe8c5dc8d05fd
Peter Schwarz <peter.schwarz@bicycle.io>
2020-07-14 14:41:12 -0500

    Implement render logic using diffed tree seq
    Implement the render logic using the new tree sequence format, after a
    diff has been applied.  This new renderer applies the changes in a
    single loop.  Its sequential nature also allows for each node to be
    visited at creation or destroy time.  The render code in the core
    namespace did not allow this case. In order to support nested
    on-mount/unmount functions, this style of rendering was required.
    
    Note, as of this commit, pseudo-tags are not yet properly supported.
    
    Signed-off-by: Peter Schwarz <peter.schwarz@bicycle.io>

---
+++ src/virtua/render.cljs
@@ -0,0 +1,116 @@
+; Copyright 2016-2020 Peter Schwarz
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+(ns virtua.render
+  (:require [clojure.core.match :refer [match]]
+            [virtua.dom :as dom]
+            [virtua.events :as evts]
+            [virtua.tree :refer [tag?]]))
+
+(defn- create-element
+  [node]
+  (when node
+    (if (tag? node)
+      (let [[tag attributes _] node
+            el (dom/create tag)]
+        (dom/set-props el attributes)
+        (evts/configure-event-handlers el attributes)
+        el)
+      (dom/create-text (str node)))))
+
+(defn- child-count [left right same]
+  (letfn [(local-count [node]
+            (if (sequential? node)
+              (last node)
+              0))]
+    (max
+      (local-count left)
+      (local-count right)
+      (local-count same))))
+
+(defn- operations
+  [left right same]
+  (match [left right same]
+    ; no chnage
+    [nil nil _] nil
+
+    [nil new-el nil]
+    [[::add new-el]]
+
+    [old-el nil nil]
+    [[::remove old-el]]
+
+    [old-el new-el nil]
+    [[::remove old-el]
+     [::add new-el]]
+
+    [([nil old-attr & more-old] :seq)
+     ([nil new-attr & more-new] :seq)
+     [tag same-attr _]]
+    [[::change-attr old-attr new-attr]]
+
+    :else nil))
+
+(defn- apply-operations [f el index ops]
+  (doseq [op ops]
+    (let [effect (first op)]
+      (case effect
+        ::remove
+        (let [child (dom/child-at el index)]
+          (f [:virtua/remove (last op) child])
+          (dom/remove! child))
+
+        ::add
+        (let [child (create-element (last op))]
+          (f [:virtua/add (last op) child])
+          (dom/insert-at el child index))
+
+        ::change-attr
+        (let [[_ remove-attrs add-attrs] op
+              child (dom/child-at el index)]
+          (dom/update-props child remove-attrs add-attrs)
+          (evts/update-event-handlers child remove-attrs add-attrs))))))
+
+(defn render
+  ([parent dom-diff] (render parent identity dom-diff))
+  ([parent f dom-diff]
+   (loop [[left right same] dom-diff
+          element-stack (cons [parent 1 0] nil)]
+       (let [node-left (first left)
+             node-right (first right)
+             node-same (first same)]
+         (when (and (or node-left node-right node-same)
+                    (not (empty? element-stack)))
+           (let [[parent nchildren index] (first element-stack)
+                 ops (operations node-left node-right node-same)
+                 _ (apply-operations f parent index ops)
+                 child (dom/child-at parent index)
+
+                 next-nchildren (child-count node-left node-right node-same)
+
+                 new-stack (cond->> (rest element-stack)
+                             (> nchildren (inc index))
+                             (cons [parent nchildren (inc index)])
+
+                             (and child (> next-nchildren 0))
+                             (cons [child next-nchildren 0]))
+
+                 advance (if (and (not child) (> next-nchildren 0))
+                           ; drop old children, if the parent was dropped
+                           next-nchildren
+                           1)]
+
+             (recur [(nthrest left advance)
+                     (nthrest right advance)
+                     (nthrest same advance)]
+                     new-stack)))))))
---
+++ test/virtua/render_test.cljs
@@ -0,0 +1,207 @@
+(ns virtua.render-test
+  (:require [cljs.test :refer-macros [deftest testing async is are use-fixtures]]
+            [virtua.dom :refer [child-at child-count]]
+            [virtua.render :as r]
+            [virtua.test-utils
+             :refer [css-class tag text-content]
+             :refer-macros [with-container]]))
+
+(deftest test-render-single-element
+  (testing "rendering a simple element"
+    (with-container el
+      (r/render el [nil '([:div nil 1] "hello") nil])
+
+      (is (= 1 (child-count el)))
+      (is (= "div" (tag (child-at el 0))))
+      (is (= "hello" (-> (child-at el 0)
+                         text-content))))))
+
+
+(deftest test-render-nested-elements
+  (testing "rendering nested elements"
+    (with-container el
+      (r/render
+        el
+        [nil
+         '([:div nil 2]
+          [:p nil 1]
+          "paragraph1"
+          [:p nil 1]
+          "paragraph2")
+        nil])
+
+      (is (= 1 (child-count el)))
+      (let [div (child-at el 0)]
+        (is (= "div" (tag div)))
+        (is (= 2 (child-count div)))
+
+        (is (= "paragraph1"
+               (-> (child-at div 0)
+                   text-content)))
+        (is (= "paragraph2"
+               (-> (child-at div 1)
+                   text-content)))))))
+
+(deftest test-render-with-tree-diff
+  (testing "rendering with simple text change"
+    (with-container el
+      ; render first with initial tree
+      (r/render el [nil '([:div nil 1] "Test Text") nil])
+      (is (= 1 (child-count el)))
+      (is (= "div" (tag (child-at el 0))))
+      (is (= "Test Text"
+             (-> (child-at el 0)
+                 text-content)))
+
+      ; render with the text changed
+      (r/render el [[nil "Test Text"] [nil "Changed Text"] [[:div nil 1]]])
+
+      (is (= "Changed Text"
+             (-> (child-at el 0)
+                 text-content))))))
+
+(deftest test-render-with-diff-additive
+  (testing "rendering with an additive difference"
+    (with-container el
+      (r/render el [nil '([:ul nil 2] [:li nil 1] "always shown") nil])
+
+      (is (= 1 (child-count el)))
+      (let [ul (child-at el 0)]
+        (is (= 1 (child-count ul)))
+        (is (= "always shown"
+               (-> (child-at ul 0)
+                   text-content)))
+
+        (r/render
+          el
+          [nil ; old
+           [nil nil nil [:li nil 1] "Now showing: additive"] ; new
+           [[:ul nil 2] [:li nil 1] "always shown"]]) ; same
+
+        (is (= 2 (child-count ul)))
+        (is (= "always shown"
+               (-> (child-at ul 0)
+                   text-content)))
+        (is (= "Now showing: additive"
+               (-> (child-at ul 1)
+                   text-content)))))))
+
+(deftest test-render-with-diff-reductive
+  (testing "rendering with a reductive difference"
+    (with-container el
+      (r/render
+        el
+        [nil
+         '([:ul nil 2]  [:li nil 1] "always shown"  [:li nil 1] "Now showing: reductive")
+         nil])
+      (is (= 1 (child-count el)))
+      (let [ul (child-at el 0)]
+        (is (= 2 (child-count ul)))
+        (is (= "always shown"
+               (-> (child-at ul 0)
+                   text-content)))
+        (is (= "Now showing: reductive"
+               (-> (child-at ul 1)
+                   text-content)))
+
+        (r/render
+         el
+         [[[nil nil 2] nil nil [:li nil 1] "Now showing: reductive"]
+         [[nil nil 1]]
+         [[:ul nil] [:li nil 1] "always shown"]])
+
+        (is (= 1 (child-count ul)))
+        (is (= "always shown"
+               (-> (child-at ul 0)
+                   text-content)))
+        (is (nil? (child-at ul 1)))))))
+
+(deftest test-render-with-diff-insertive
+  (testing "rendering with an insertive difference"
+    (with-container el
+      (r/render
+        el
+        [nil '([:ul nil 1] [:li nil 1] "always shown") nil])
+
+      (is (= 1 (child-count el)))
+      (let [ul (child-at el 0)]
+        (is (= 1 (child-count ul)))
+        (is (= "always shown"
+               (-> (child-at ul 0)
+                   text-content)))
+
+        (r/render
+          el
+          [[[nil nil 1] nil "always shown"]
+           [[nil nil 2] nil "Now showing: insertive" [:li nil 1] "always shown"]
+           [[:ul nil] [:li nil 1]]])
+
+        (is (= 2 (child-count ul)))
+        (is (= "Now showing: insertive"
+               (-> (child-at ul 0)
+                   text-content)))
+        (is (= "always shown"
+               (-> (child-at ul 1)
+                   text-content)))))))
+
+(deftest test-render-with-diff-attribute-change
+  (testing "rendering with an attribute difference"
+    (with-container el
+      (r/render
+        el
+        [nil
+         '([:p  {:class "text-success"} 1] "Text")])
+
+      (is (= 1 (child-count el)))
+      (let [p (child-at el 0)]
+        (is (= "text-success" (css-class p)))
+        (is (= "Text" (text-content p)))
+
+        (r/render
+          el
+          [[[nil {:class "text-success"}]]
+           [[nil {:class "text-danger"}]]
+           [[:p nil 1] "Text"]])
+
+        (is (= "text-danger" (css-class p)))
+        (is (= "Text" (text-content p)))))))
+
+(deftest test-render-emit
+  (testing "render with an add emit"
+    (with-container el
+      (let [events (atom [])]
+        (r/render
+          el
+          #(swap! events conj %)
+          [nil
+           '([:h1 nil 1] "Text")])
+
+        (let [[event el-decl el] (first @events)]
+          (is (= :virtua/add event))
+          (is (= [:h1 nil 1] el-decl))
+          (is (= "h1" (tag el)))))))
+
+  (testing "render with remove emit"
+    (with-container el
+      (let [events (atom [])]
+        (r/render
+          el
+          [nil
+           '([:h1 nil 1] "Text")])
+        ; remove it
+        (r/render
+          el
+          #(swap! events conj %)
+          [[[:h1 nil 1] "Text"]
+           [[:div nil 1] "More text"]
+           nil])
+
+        (let [[event el-decl el] (first @events)]
+          (is (= :virtua/remove event))
+          (is (= [:h1 nil 1] el-decl))
+          (is (= "h1" (tag el))))
+
+        (let [[event el-decl] (get @events 1)]
+          (is (= :virtua/add event))
+          (is (= [:div nil 1] el-decl))
+          (is (= "div" (tag el))))))))
